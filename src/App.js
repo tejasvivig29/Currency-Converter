4b52aa78bf9779db88dd19fa9ad5ff2c5e4b407f
Tejasvi Vig <tejasvivig09@gmail.com>
2020-06-27 14:06:09 -0300

    Commiting the source code for App.js

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
+
+function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [amount,setAmount] = useState(1)
+  const [amountInFromCurrency,setamountInFromCurrency] = useState(true)
+  const [exchangeRate,setExchangeRate] = useState() 
+
+
+  let toAmount, fromAmount
+  if(amountInFromCurrency){
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else{
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
+
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+          const firstCurrency = Object.keys(data.rates)[0] 
+          setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+          setFromCurrency(data.base)
+          setToCurrency(firstCurrency)
+          setExchangeRate(data.rates[firstCurrency])
+        })
+  }, [])
+
+  function handleFromAmountChange(e){
+    setAmount(e.target.value)
+    setamountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e){
+    setAmount(e.target.value)
+    setamountInFromCurrency(false)
+  }
+
+  return (
+    <div>
+        <h1>Convert</h1>
+        <CurrencyRow 
+           currencyOptions={currencyOptions}
+           selectedCurrency={fromCurrency}
+           onChangeCurrency={e => setFromCurrency(e.target.value)}
+           onChangeAmount={handleFromAmountChange} 
+           amount={fromAmount}
+        />
+        <div className="equals">=</div>
+        <CurrencyRow 
+           currencyOptions={currencyOptions}
+           selectedCurrency={toCurrency}
+           onChangeCurrency={e => setToCurrency(e.target.value)}
+           onChangeAmount={handleToAmountChange}
+           amount={toAmount}
+        />
+    </div>
+  );
+}
+
+export default App;
